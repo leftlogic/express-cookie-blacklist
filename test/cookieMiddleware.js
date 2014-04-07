f5d315d57b7e2300a2b2de2bf06a0e1102b22fe8
Fabien O'Carroll <fabien@allou.is>
2014-04-07 13:08:15 +0100

    update dalek test to run properly

--- test/cookieMiddleware.js
+++ test/cookieMiddleware.js
@@ -1,9 +1,8 @@
 module.exports = {
-  'should not send back cookie data that\'s in the blacklist': function(){
-    dalek.open('localhost:8462')
-      .assert.cookie('test', 'okay=you can see me')
-      .done();
-    dalek.open('localhost:8462/close')
+  'should not send back cookie data that\'s in the blacklist': function(test){
+    test.open('localhost:8462')
+      .assert.cookie('test', 'okay=youcanseeme')
+      .open('localhost:8462/close')
       .done();
   }
 };
