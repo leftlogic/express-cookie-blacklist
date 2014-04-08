aea2ca13589d0877a276fddc7df81e6bf7fca33c
Fabien O'Carroll <fabien@allou.is>
2014-04-08 16:46:47 +0100

    use connect passed to module

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var connect= require('connect');
-var cookieSession = connect.cookieSession;
 var deleteItem = require('./lib/deleteItem');
 
-module.exports = function (options) {
-  var cookieSessionMiddleware = cookieSession.apply(connect, arguments); 
-  var blacklist = options.blacklist;
-  return function (req, res, next) {
-    res.on('header', function () {
-      if (blacklist) {
-        blacklist.forEach(function(key) {
-          deleteItem(req.session, key);
-        });
-      }
-    });
-    return cookieSessionMiddleware(req, res, next);
+module.exports = function (connect) {
+  return function (options) {
+    var cookieSessionMiddleware = connect.cookieSession.apply(connect, arguments);
+    var blacklist = options.blacklist;
+    return function (req, res, next) {
+      res.on('header', function () {
+        if (blacklist) {
+          blacklist.forEach(function(key) {
+            deleteItem(req.session, key);
+          });
+        }
+      });
+      return cookieSessionMiddleware(req, res, next);
+    };
   };
 };
