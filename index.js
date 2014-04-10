c8d13c1e5e914dd3a7336bc9c0e6711b5ad0156b
Fabien O'Carroll <fabien@allou.is>
2014-04-10 13:40:52 +0100

    Add _sessionBeforeBlacklist for testing session in res.on('header'

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function (connect) {
     var cookieSessionMiddleware = connect.cookieSession.apply(connect, arguments);
     var blacklist = options.blacklist;
     return function (req, res, next) {
+      req._sessionBeforeBlacklist = JSON.parse(JSON.stringify(req.session));
       res.on('header', function () {
         if (blacklist) {
           blacklist.forEach(function(key) {
