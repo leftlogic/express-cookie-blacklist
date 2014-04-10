16ae9528ab0dd8c58783465026890e3825ae1dee
Remy Sharp <remy@remysharp.com>
2014-04-10 15:14:40 +0100

    Turns out _.clone was actually lying...

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var deleteItem = require('./lib/deleteItem');
-var _ = require('underscore');
+var clone = require('clone');
 
-module.exports = function (connect) {
-  return function (options) {
-    var cookieSessionMiddleware = connect.cookieSession.apply(connect, arguments);
-    var blacklist = options.blacklist;
-    return function (req, res, next) {
-      res.on('header', function () {
-        req._sessionBeforeBlacklist = _.clone(req.session);
-        if (blacklist) {
-          blacklist.forEach(function(key) {
-            deleteItem(req.session, key);
-          });
-        }
-      });
-      return cookieSessionMiddleware(req, res, next);
-    };
+module.exports = function (blacklist) {
+  'use strict';
+  if (blacklist && !Array.isArray(blacklist)) {
+    blacklist = [blacklist];
+  }
+  return function (req, res, next) {
+    res.on('header', function () {
+      req._sessionBeforeBlacklist = clone(req.session);
+      if (blacklist) {
+        blacklist.forEach(function(key) {
+          deleteItem(req.session, key);
+        });
+      }
+    });
+    next();
   };
 };
